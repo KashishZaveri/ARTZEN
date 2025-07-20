3604f48ff7bfae66ecdb6de626ee1d9152deb831
Kashish Zaveri <kashishzaveri023@gmail.com>
2025-07-20 20:38:33 +0530

    format all code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ["ARTZEN.onrender.com"],
+    allowedHosts: ["artzen.onrender.com"],
   },
   server: {
     proxy: {
